7e86dd00631d4410554eb47bc2105d8a371a18fb
Horckun <horckun@gmail.com>
2019-05-12 19:16:22 +0200

    001 - Hola mundo!
    Iniciando PHP

---
+++ 001 - Hola mundo.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="es" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Hola mundo!</title>
+  </head>
+  <body>
+    <h1>Hola mundo en PHP</h1>
+    <?php
+      print "Hola mundo!";
+      phpinfo();
+    ?>
+  </body>
+</html>
