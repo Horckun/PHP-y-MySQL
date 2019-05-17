bca130acf1bd549adba7b96da66a387ed64e6403
Horckun <horckun@gmail.com>
2019-05-17 10:08:46 +0200

    Leer archivo
    Leer archivo en PHP

---
+++ 002 - Leer archivo/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="es" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Leer archivo</title>
+  </head>
+  <body>
+    <h1>Leer archivo</h1>
+    <?php
+      readfile("archivo.txt");
+    ?>
+  </body>
+</html>
