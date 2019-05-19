3aa7571877257e91ef14dfdd27f5941f60da28e5
Horckun <horckun@gmail.com>
2019-05-19 11:43:08 +0200

    Variables
    Declarar y mostrar variables

---
+++ 003 - Variables/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="es" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Variables</title>
+  </head>
+  <body>
+    <!-- Título -->
+    <h1>Variables</h1>
+    <!-- PHP -->
+    <?php
+      // Declarar variables
+      $nombre = "Horckun";
+      $edad = 30;
+
+      // Mostrar en pantalla
+      print "$nombre tiene $edad años.";
+    ?>
+  </body>
+</html>
