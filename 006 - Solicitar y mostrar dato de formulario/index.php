d2dce3b242578b3e3177c5ad3171c697d54a3664
Horckun <horckun@gmail.com>
2019-05-26 09:54:28 +0200

    Solicitar dato de formulario
    Solicitar y mostrar dato en formulario por método get ne PHP

---
+++ 006 - Solicitar y mostrar dato de formulario/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="es" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Solicitar y mostrar dato de formulario</title>
+  </head>
+  <body style="width: 40%; margin: 0 auto; text-align: center;">
+    <h1 style="text-align: center;">Solicitar y mostrar dato de formulario</h1>
+    <!-- FORMULARIO - ENVIAR DATOS A DATO.PHP POR MÉTODO GET -->
+    <form class="" action="dato.php" method="get" style="width: 40%; margin: 0 auto; background: #eee;">
+      <fieldset>
+        <label>Inserta tu nombre:</label>
+        <br>
+        <input type="text" name="nombre" value="" placeholder="Insertar nombre..." autofocus required>
+        <hr>
+        <input type="submit" name="enviar" value="Enviar formulario">
+      </fieldset>
+    </form>
+  </body>
+</html>
