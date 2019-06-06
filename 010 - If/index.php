b628228923bb8143b92863f18a9d3967b0e831cf
Horckun <horckun@gmail.com>
2019-06-06 13:49:19 +0200

    If
    Condicional If

---
+++ 010 - If/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="es" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Números aleatorios</title>
+  </head>
+  <body>
+    <h1>Números aleatorios</h1>
+    <!-- CÓDIGO PHP -->
+    <?php
+      // DECLARAR VARIABLE ALEATORIA
+      $aleatorio = rand(1,6);
+      // HEREDOCS
+      print <<<HERE
+      <p>Has sacado $aleatorio.</p>
+      <p><img src="img/$aleatorio.png"></p>
+HERE;
+      /* if (condition) {
+        // code...
+      }*/
+      if($aleatorio == 6) {
+        print "<h2>¡¡¡Enhorabuena!!!</h2>";
+      }
+      /*
+      == IGUAL A
+      < MENOR QUE
+      > MAYOR QUE
+      <= MENOR O IGUAL QUE
+      >= MAYOR O IGUAL QUE
+      != DISTINTO DE
+      */
+    ?>
+  </body>
+</html>
