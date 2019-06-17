739d2f10f2c855e58c7f015c79f5988cf3081885
Horckun <horckun@gmail.com>
2019-06-17 14:53:49 +0200

    Variables globales
    Declarar variables globales

---
+++ 018 - Variables globales/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="es" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Variables globales</title>
+  </head>
+  <body>
+    <h1>Variables globales</h1>
+    <!-- PHP -->
+    <?php
+      // DECLARAR VARIABLES GLOBALES Y ASIGNARLES VALORES
+      $x = rand(0,100);
+      $y = rand(0,100);
+
+      // MOSTRAR EN PANTALLA
+      print "<h2>X vale $x e Y vale $y.</h2>";
+    ?>
+  </body>
+</html>
