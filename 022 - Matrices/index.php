693c416de850c8e8e92f1729a2ff6fbf0d8e1fcf
Horckun <horckun@gmail.com>
2019-06-21 10:22:03 +0200

    Matrices
    Crear matrices

---
+++ 022 - Matrices/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="es" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Matrices</title>
+  </head>
+  <body>
+    <h1>Matrices</h1>
+    <!-- PHP -->
+    <?php
+      // Declarar variables de matrices y asignarles valores
+      $color[0] = "azul";
+      $color[1] = "negro";
+      $color[2] = "blanco";
+
+      // Mostrar en pantalla
+      print "Me gusta el fondo $color[0] con un texto $color[2] y con sombra de color $color[1].";
+    ?>
+  </body>
+</html>
