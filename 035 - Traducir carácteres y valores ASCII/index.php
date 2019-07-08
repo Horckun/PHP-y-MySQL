e53762a1c6d0b4ee8e3cfaf08c236cc7d089c460
Horckun <horckun@gmail.com>
2019-07-08 15:58:49 +0200

    035
    Traducir carácter a ASCII

---
+++ 035 - Traducir carácteres y valores ASCII/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="es" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Traducir carácteres y valores ASCII</title>
+  </head>
+  <body>
+    <h1>Traducir carácteres y valores ASCII</h1>
+    <!-- PHP -->
+    <?php
+      // Declarar variable
+      $letra = ord("H");
+      print "<h2>Letra: $letra.</h2>";
+    ?>
+  </body>
+</html>
